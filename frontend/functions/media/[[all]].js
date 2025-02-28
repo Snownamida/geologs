cd347cfb68da6bbfb6d9cb94be1cc96566c69159
Jixiang SUN <snownamida@outlook.com>
2025-02-28 23:34:52 +0100

    得，和教程完全一样

--- frontend/functions/media/[[all]].js
+++ frontend/functions/media/[[all]].js
@@ -1,6 +1,6 @@
 export async function onRequestGet(ctx) {
-  const path = new URL(ctx.request.url).pathname.replace("/files/", "");
-  const file = await ctx.env.FILES.get(path);
+  const path = new URL(ctx.request.url).pathname.replace("/media/", "");
+  const file = await ctx.env.MEDIA.get(path);
   if (!file) return new Response(null, { status: 404 });
   return new Response(file.body, {
     headers: { "Content-Type": file.httpMetadata.contentType },
